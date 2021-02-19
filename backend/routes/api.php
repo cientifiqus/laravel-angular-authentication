d3a8c4758bd8041ed485baf9bd224792a5d5de31
Montolla <montolla@montolla.tk>
2021-02-19 18:19:26 -0500

    Login form added and CORS for all origins

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,37 +1,16 @@
+
 <?php
+
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-
 Route::group([
-
     'middleware' => 'api',
-    'prefix' => 'auth'
-
-],
-    function () {
-        //function ($router) {
-
-        Route::post('login', 'AuthController@login');
-        Route::post('logout', 'AuthController@logout');
-        Route::post('refresh', 'AuthController@refresh');
-        Route::post('me', 'AuthController@me');
-    }
-);
+], function () {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::get('mio', 'App\Http\Controllers\AuthController@me');
+});
